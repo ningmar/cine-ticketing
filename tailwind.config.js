a38f27bca966250496bad04d53195ec3f52a7928
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2021-03-24 22:29:36 +0545

    modified tailwind config to extend font instead of override

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      'HK' : [ 'HK Grotesk', 'sans-serif' ],
+    extend: {
+      fontFamily: {
+        'sans' : ['"HK Grotesk"', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {}
