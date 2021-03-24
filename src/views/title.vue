b025bebd311f8c14ae894a641dd7ddb662092158
Nikesh Pancha <35023459+nickeshh@users.noreply.github.com>
2021-03-24 08:46:25 +0545

    title but in new page garna ayana malai

---
+++ src/views/title.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="col-xl-8 mx-auto my-5 mb-8 text-center ">
+        <h2 class="text-5xl uppercase tracking-widest font-HK font-bold">- Cartelera Artesiete -</h2>  
+    </div>    
+</template>
+
