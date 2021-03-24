a0c83067a854a034b2eb321511133aeee10f0f12
Nikesh Pancha <35023459+nickeshh@users.noreply.github.com>
2021-03-24 20:10:14 +0545

    Createed SectionTitle component

---
+++ src/components/ui/SectionTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col-xl-8 mx-auto my-5 mb-8 text-center ">
+        <h2 class="text-5xl uppercase tracking-widest font-HK font-bold">- {{ title }} -</h2>  
+    </div> 
+</template>
+
+<script>
+export default {
+    name: "SectionTitle",
+    props: {
+        title: String
+    },
+}
+</script>
+
+<style>
+
+</style>
