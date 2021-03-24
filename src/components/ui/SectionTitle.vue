918bdbe32ba8057ccb5998ca82377214023ca125
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2021-03-24 22:41:28 +0545

    fixed styling

--- src/components/ui/SectionTitle.vue
+++ src/components/ui/SectionTitle.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="col-xl-8 mx-auto my-5 mb-8 text-center ">
-        <h2 class="text-5xl uppercase tracking-widest font-HK font-bold">- {{ title }} -</h2>  
+    <div class="mb-12 text-center">
+        <h2 class="text-4xl uppercase tracking-widest font-bold">- {{ title }} -</h2>  
     </div> 
 </template>
 
