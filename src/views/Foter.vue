277aacf7d04e4dd8c7e1f0409d0ae5fce269d4ce
Nikesh Pancha <35023459+nickeshh@users.noreply.github.com>
2021-03-25 21:09:14 +0545

    adding Footer section

--- src/views/Foter.vue
+++ src/views/Foter.vue
@@ -1,6 +1,31 @@
 <template>
-    <div class="container max-w-7xl mx-auto">
-        <div class="flex flex-wrap bg-gray-100 py-24 divide-x divide-black">
+    <div 
+       class="container max-w-7xl mx-auto"
+    >
+        <div 
+            class="flex flex-wrap bg-gray-100 px-4 py-10 md:py-17 lg:py-24 md:divide-x divide-black"
+        >
+            <div 
+                class="flex md:flex-1 justify-center"
+            >
+                <svg 
+                    class="svg-icon service-icon w-16 h-16"
+                >
+                </svg>
+                <div 
+                    class="service-text text-left"
+                >
+                    <h2 
+                        class="uppercase font-HK font-bold text-base"
+                    >Pago Seguro
+                    </h2>
+                    <p 
+                        class="font-HK text-gray-400 text-xs"
+                    >Pago Seguro, Operacion realizada en Espana
+                    </p>
+                </div>
+            </div>
+
             <div class="flex md:flex-1 justify-center">
                 <svg class="svg-icon service-icon w-16 h-16"></svg>
                 <div class="service-text text-left">
@@ -8,11 +33,100 @@
                     <p class="font-HK text-gray-400 text-xs">Pago Seguro, Operacion realizada en Espana</p>
                 </div>
             </div>
-            <div class="flex md:flex-1 justify-center">
-                <svg class="svg-icon service-icon w-16 h-16"></svg>
-                <div class="service-text text-left">
-                    <h2 class="uppercase font-HK font-bold text-base">Pago Seguro</h2>
-                    <p class="font-HK text-gray-400 text-xs">Pago Seguro, Operacion realizada en Espana</p>
+        </div>
+        <!-- for main footer section -->
+        <div class="bg-gray-200 py-10 md:py-15 lg:py-20">
+            <div
+                class="flex flex-wrap px-4 lg:mx-36 md:mx-10"
+                >
+                <div class="md:flex-1">
+                    <h3 class="text-gray-900 uppercase font-HK text-base tracking-widest font-bold my-4">
+                        Artesiete cines
+                    </h3>
+                </div>
+                <div class="md:flex-1">
+                    <h3 class="text-gray-900 uppercase font-HK text-base tracking-widest my-4">
+                        Nuestra Empres
+                    </h3>
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Comprueba Compra</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Acceso a la Sala</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Contacto Empresa</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Trabaja con Nosotros</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Política de Cookies</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Aviso Legal</a
+                    >
+                </div>
+                <div class="md:flex-1">
+                    <h3 class="text-gray-900 uppercase font-HK text-base tracking-widest my-4">
+                        Nuestra Empres
+                    </h3>
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Comprueba Compra</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Acceso a la Sala</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Contacto Empresa</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Trabaja con Nosotros</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Política de Cookies</a
+                    >
+                    <a 
+                        href="#"
+                        class="block text-sm leading-6 font-HK hover:underline text-gray-400"
+                        >Aviso Legal</a
+                    >
+                </div>
+            </div>
+        </div>
+
+        <!-- for bottom footer section -->
+        <div class="bg-gray-800 py-3 md:py-6">
+            <div
+                class="flex flex-wrap px-4 lg:mx-36 md:mx-10"
+                >
+                <div class="md:flex-1">
+                    <p class="text-sm text-gray-300">© 2018 Artesiete.  All rights reserved.</p>
+                </div>
+                <div class="md:flex-1">
+                    <p class="text-sm text-right text-gray-300">Operativa Bancaria realizada en España</p>
                 </div>
             </div>
         </div>
