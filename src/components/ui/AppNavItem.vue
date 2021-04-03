a972582f5e884abb02db607ff5a5dc29d83a1e2d
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2021-04-03 19:52:00 +0545

    added router link active class for nav item

--- src/components/ui/AppNavItem.vue
+++ src/components/ui/AppNavItem.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-link :to="to" class="block py-2 group lg:px-4">
+  <router-link
+    :to="to"
+    class="block py-2 group lg:px-4"
+    active-class="text-gray-900"
+  >
     <span class="align-middle group-hover:text-gray-900">
       <!-- pass slot to override following text -->
       <slot>Nav Item</slot>
