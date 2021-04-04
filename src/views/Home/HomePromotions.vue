967d857ce3b00cd825291a9fe43b1d6f8ed366b7
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2021-04-04 16:22:03 +0545

    created HomePromotions section component

---
+++ src/views/Home/HomePromotions.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="px-1">
+    <app-section-title>Promotions</app-section-title>
+  </section>
+</template>
+
+<script>
+import AppSectionTitle from '../../components/ui/AppSectionTitle'
+
+export default {
+  name: 'HomePromotions',
+  components: {
+    AppSectionTitle
+  }
+}
+</script>
