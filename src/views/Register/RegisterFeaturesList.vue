d6fb121ff432b503738f994775e90a58ef9bd28d
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2021-04-10 19:54:33 +0545

    created features list component

---
+++ src/views/Register/RegisterFeaturesList.vue
@@ -0,0 +1,82 @@
+<template>
+  <section>
+    <p class="px-8 text-xs text-center text-gray-400 py-9 bg-gray-50">
+      <br />
+      Comienza a disfrutar las ventajas de la tarjeta Artesiete
+    </p>
+    <ul class="text-sm">
+      <li class="px-5 py-3 border border-gray-200 hover:bg-gray-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          class="inline w-4.5 h-4.5 mr-2 text-gray-900 stroke-current stroke-4"
+        >
+          <path
+            d="M50.9 55.3a12.2 12.2 0 00-4.3-1.7c-8.1-1.5-8.1-4.1-8.1-5.7v-1a14.8 14.8 0 004.2-7.1c2.5 0 3-5.1 3-5.9s.1-3.8-2.4-3.8c5.4-14.6-8.9-20.5-19.6-13-4.4 0-4.8 6.5-3.1 13-2.5 0-2.4 3.1-2.4 3.8s.6 5.9 3 5.9a14.8 14.8 0 004.3 7.2v1c0 1.6 0 4.2-8.1 5.7a11.9 11.9 0 00-4.3 1.6"
+            fill="none"
+          />
+          <circle cx="32" cy="32" r="30" fill="none" />
+        </svg>
+        <span>Consigue el Mejor Precio</span>
+      </li>
+      <li class="px-5 py-3 border border-gray-200 hover:bg-gray-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          class="inline w-4.5 h-4.5 mr-2 text-gray-900 stroke-current stroke-4"
+        >
+          <path
+            d="M50.9 55.3a12.2 12.2 0 00-4.3-1.7c-8.1-1.5-8.1-4.1-8.1-5.7v-1a14.8 14.8 0 004.2-7.1c2.5 0 3-5.1 3-5.9s.1-3.8-2.4-3.8c5.4-14.6-8.9-20.5-19.6-13-4.4 0-4.8 6.5-3.1 13-2.5 0-2.4 3.1-2.4 3.8s.6 5.9 3 5.9a14.8 14.8 0 004.3 7.2v1c0 1.6 0 4.2-8.1 5.7a11.9 11.9 0 00-4.3 1.6"
+            fill="none"
+          />
+          <circle cx="32" cy="32" r="30" fill="none" />
+        </svg>
+        <span>Acumula Puntos</span>
+      </li>
+      <li class="px-5 py-3 border border-gray-200 hover:bg-gray-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          class="inline w-4.5 h-4.5 mr-2 text-gray-900 stroke-current stroke-4"
+        >
+          <path
+            d="M50.9 55.3a12.2 12.2 0 00-4.3-1.7c-8.1-1.5-8.1-4.1-8.1-5.7v-1a14.8 14.8 0 004.2-7.1c2.5 0 3-5.1 3-5.9s.1-3.8-2.4-3.8c5.4-14.6-8.9-20.5-19.6-13-4.4 0-4.8 6.5-3.1 13-2.5 0-2.4 3.1-2.4 3.8s.6 5.9 3 5.9a14.8 14.8 0 004.3 7.2v1c0 1.6 0 4.2-8.1 5.7a11.9 11.9 0 00-4.3 1.6"
+            fill="none"
+          />
+          <circle cx="32" cy="32" r="30" fill="none" />
+        </svg>
+        <span>Consigue Regalos</span>
+      </li>
+      <li class="px-5 py-3 border border-gray-200 hover:bg-gray-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          class="inline w-4.5 h-4.5 mr-2 text-gray-900 stroke-current stroke-4"
+        >
+          <path
+            d="M50.9 55.3a12.2 12.2 0 00-4.3-1.7c-8.1-1.5-8.1-4.1-8.1-5.7v-1a14.8 14.8 0 004.2-7.1c2.5 0 3-5.1 3-5.9s.1-3.8-2.4-3.8c5.4-14.6-8.9-20.5-19.6-13-4.4 0-4.8 6.5-3.1 13-2.5 0-2.4 3.1-2.4 3.8s.6 5.9 3 5.9a14.8 14.8 0 004.3 7.2v1c0 1.6 0 4.2-8.1 5.7a11.9 11.9 0 00-4.3 1.6"
+            fill="none"
+          />
+          <circle cx="32" cy="32" r="30" fill="none" />
+        </svg>
+        <span
+          >Utiliza también tarjeta que te llegará al email en taquillas</span
+        >
+      </li>
+      <li class="px-5 py-3 border border-gray-200 hover:bg-gray-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          class="inline w-4.5 h-4.5 mr-2 text-gray-900 stroke-current stroke-4"
+        >
+          <path
+            d="M50.9 55.3a12.2 12.2 0 00-4.3-1.7c-8.1-1.5-8.1-4.1-8.1-5.7v-1a14.8 14.8 0 004.2-7.1c2.5 0 3-5.1 3-5.9s.1-3.8-2.4-3.8c5.4-14.6-8.9-20.5-19.6-13-4.4 0-4.8 6.5-3.1 13-2.5 0-2.4 3.1-2.4 3.8s.6 5.9 3 5.9a14.8 14.8 0 004.3 7.2v1c0 1.6 0 4.2-8.1 5.7a11.9 11.9 0 00-4.3 1.6"
+            fill="none"
+          />
+          <circle cx="32" cy="32" r="30" fill="none" />
+        </svg>
+        <span>También puedes instalarte nuestra APP</span>
+      </li>
+    </ul>
+  </section>
+</template>
