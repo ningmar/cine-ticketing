8a42aa5d86895fe63aed529f222142cd849b1b52
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2021-04-10 15:50:15 +0545

    added common ui components

---
+++ src/components/ui/AppInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mb-4 text-gray-400">
+    <label v-if="label" class="block mb-2 text-xs tracking-widest uppercase">
+      {{ label }}
+      <span v-if="required">*</span>
+    </label>
+    <input
+      :value="value"
+      :type="type"
+      :placeholder="placeholder"
+      class="w-full px-3 py-2.5 text-sm border border-gray-300 focus:ring-1 focus:ring-gray-500 focus:outline-none"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppInput',
+  props: {
+    value: { type: String, required: false, default: '' },
+    label: { type: String, required: false, default: '' },
+    placeholder: { type: String, required: false, default: '' },
+    type: { type: String, required: false, default: 'text' },
+    required: { type: Boolean, required: false, default: false }
+  }
+}
+</script>
