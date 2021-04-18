81362ed76e239a9cd44f53e104fee02a65dab67d
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2021-04-18 13:35:28 +0545

    decouple header and footer contents into multiple components

---
+++ src/components/layouts/Footer/FooterServices.vue
@@ -0,0 +1,78 @@
+<template>
+  <section class="py-16 md:py-24 bg-gray-50">
+    <div class="max-w-6xl mx-auto">
+      <div class="md:flex md:divide-x md:divide-gray-900">
+        <div
+          class="flex items-center px-4 mb-4 md:mb-0 md:justify-center md:w-1/2"
+        >
+          <svg
+            class="flex-shrink-0 w-12 h-12 mr-6 text-gray-800 stroke-current stroke-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 64 64"
+          >
+            <path
+              fill="none"
+              d="M38 42H4.4A2.4 2.4 0 012 39.6V12.4A2.4 2.4 0 014.4 10h43.2a2.4 2.4 0 012.4 2.4V22M2 18h48m-39.9 8H26m-16 8h7"
+            />
+            <path
+              fill="none"
+              d="M38 38h24v16H38zm6 0V28a6 6 0 016-6 6 6 0 016 6v10m-6 6v4"
+            />
+          </svg>
+          <div>
+            <h6
+              class="text-sm font-bold tracking-widest text-gray-900 uppercase"
+            >
+              Pago Seguro
+            </h6>
+            <p class="mt-1 text-xs text-gray-400">
+              Pago Seguro, Operación realizada en España
+            </p>
+          </div>
+        </div>
+        <div class="flex items-center px-4 md:justify-center md:w-1/2">
+          <svg
+            class="flex-shrink-0 w-12 h-12 mr-6 text-gray-800 stroke-current stroke-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 64 64"
+          >
+            <path
+              d="M24 40.5V44c0 2 .6 5-10.4 7A14.6 14.6 0 002 62h59a14.3 14.3 0 00-11.4-11C39 49 40 46 40 44v-5"
+              fill="none"
+            />
+            <path
+              d="M46.4 33H51a3.3 3.3 0 003-3v-7a3.3 3.3 0 00-3-3h-5m-28 0h-6a3.3 3.3 0 00-3 3v7a3.3 3.3 0 003 3h5.2"
+              fill="none"
+            />
+            <path
+              d="M46 18C26 18 26 4 26 4s1 12-8 15m18 43l9.6-12m-27.7 0L28 62M17.9 50a10.8 10.8 0 01-4.9-4c4-4 5-20 5-20m28 0s0 16 4 20a10.7 10.7 0 01-4.7 4"
+              fill="none"
+            />
+            <path
+              d="M26 42c-4.7-2.9-8-8.8-8-16V16C18 5.6 25.4 2 32 2s14 3.6 14 14v16c0 4-4 7-6 7"
+              fill="none"
+            />
+            <path
+              d="M40 38.9a2.9 2.9 0 00-2.9-2.9h-2.2a2.9 2.9 0 00-2.9 2.9 3 3 0 002.9 3.1H37a3 3 0 003-3.1z"
+              fill="none"
+            />
+          </svg>
+          <div>
+            <h6
+              class="text-sm font-bold tracking-widest text-gray-900 uppercase"
+            >
+              En nuestro formulario de contacto
+            </h6>
+            <p class="mt-1 text-xs text-gray-400">Respuesta Asegurada</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'FooterServices'
+}
+</script>
---
+++ src/components/layouts/Footer/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <footer-services></footer-services>
+    <footer-links></footer-links>
+    <footer-copyright></footer-copyright>
+  </footer>
+</template>
+
+<script>
+import FooterCopyright from './FooterCopyright'
+import FooterLinks from './FooterLinks'
+import FooterServices from './FooterServices'
+
+export default {
+  name: 'AppFooter',
+  components: {
+    FooterCopyright,
+    FooterLinks,
+    FooterServices
+  }
+}
+</script>
